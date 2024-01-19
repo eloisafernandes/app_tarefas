a4fb3829da816e8244660f430db0bbae6fc469a6
eloisafernandes <eloisafer16@gmail.com>
2024-01-19 16:20:04 +0000

    cores e titulo

---
+++ lib/todo_list_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+class _TodoListPageState extends State<TodoListPage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("To Do App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Task 1',
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
